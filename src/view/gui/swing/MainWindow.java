cd2ec33b8707a7700700a989d5dae8ffd2bba929
hecoding <hlaria@ucm.es>
2016-02-28 08:12:34 +0100

    gui

--- src/view/gui/swing/MainWindow.java
+++ src/view/gui/swing/MainWindow.java
@@ -34,9 +34,9 @@ public class MainWindow extends JFrame {
 		//----------Content-----------
 		
 		leftPanel.setBackground(Color.BLUE);
-		final ConfigPanel<Caca> cp = creaPanelConfiguracion();
-		Caca caca = new Caca();
-		cp.setTarget(caca);
+		final ConfigPanel<Modelo> cp = creaPanelConfiguracion();
+		Modelo modelo = new Modelo();
+		cp.setTarget(modelo);
 		cp.initialize();
 		leftPanel.add(cp);
 		// meter quiza un JScrollPane y/o JSplitPane
@@ -98,70 +98,150 @@ public class MainWindow extends JFrame {
 		this.setVisible(true);
 	}
 	
-	public ConfigPanel<Caca> creaPanelConfiguracion() {
-		String[] colores = new String[] { "yeah", "nope", "maybe" };
+	public ConfigPanel<Modelo> creaPanelConfiguracion() {
+		String[] funciones = new String[] { "función 1", "función 2", "función 3", "función 4", "función 5" };
 		Forma[] formas = new Forma[] { new Rectangulo() };
 		
-		ConfigPanel<Caca> config = new ConfigPanel<Caca>();
+		ConfigPanel<Modelo> config = new ConfigPanel<Modelo>();
 		
 		// se pueden a�adir las opciones de forma independiente, o "de seguido"; el resultado es el mismo.
-		config.addOption(new ConfigPanel.IntegerOption<Caca>(  // -- entero
-				"grosor (px)", 					     // texto a usar como etiqueta del campo
-				"pixeles de grosor del borde",       // texto a usar como 'tooltip' cuando pasas el puntero
-				"grosor",  						     // campo (espera que haya un getGrosor y un setGrosor)
-				1, 10))							     // min y max (usa Integer.MIN_VALUE /MAX_VALUE para infinitos)
-		.addOption(new ConfigPanel.ChoiceOption<Caca>(	 // -- eleccion de objeto no-configurable
-			    "color",							 // etiqueta 
-			    "color del borde", 					 // tooltip
-			    "color",   							 // campo (debe haber un getColor y un setColor)
-			    colores))                            // elecciones posibles
-		.addOption(new ConfigPanel.StrategyOption<Caca>( // -- eleccion de objeto configurable
+		config.addOption(new ConfigPanel.ChoiceOption<Modelo>(	 // -- eleccion de objeto no-configurable
+			    "Función",							 // etiqueta 
+			    "función a optimizar",				 // tooltip
+			    "funcion",   							 // campo (debe haber un getColor y un setColor)
+			    funciones))                            // elecciones posibles
+		
+		.addOption(new ConfigPanel.DoubleOption<Modelo>(   // -- doble, parecido a entero
+			    "Precisión",	 					 // etiqueta
+			    "bla",				 // tooltip
+			    "precision",	                     // campo
+			    0, Double.POSITIVE_INFINITY))	     // min y max, aplicando factor, si hay; vale usar Double.*_INFINITY)
+		  		  
+		.addOption(new ConfigPanel.IntegerOption<Modelo>(  // -- entero
+				"Población", 					     // texto a usar como etiqueta del campo
+				"bla",	         // texto a usar como 'tooltip' cuando pasas el puntero
+				"poblacion",					     // campo (espera que haya un getGrosor y un setGrosor)
+				1, Integer.MAX_VALUE))			     // min y max (usa Integer.MIN_VALUE /MAX_VALUE para infinitos)
+		.addOption(new ConfigPanel.IntegerOption<Modelo>(  // -- entero
+				"Generaciones",					     // texto a usar como etiqueta del campo
+				"bla",       // texto a usar como 'tooltip' cuando pasas el puntero
+				"generaciones",					     // campo (espera que haya un getGrosor y un setGrosor)
+				1, Integer.MAX_VALUE))			     // min y max (usa Integer.MIN_VALUE /MAX_VALUE para infinitos)
+		.addOption(new ConfigPanel.DoubleOption<Modelo>(   // -- doble, parecido a entero
+			    "% cruces", 						 // etiqueta
+			    "bla",           // tooltip
+			    "porcCruces",                     // campo
+			    0, 100,							     // min y max, aplicando factor, si hay; vale usar Double.*_INFINITY) 
+			    100))								 // opcional: factor de multiplicacion != 1.0, para mostrar porcentajes
+		.addOption(new ConfigPanel.DoubleOption<Modelo>(   // -- doble, parecido a entero
+			    "% mutación", 						 // etiqueta
+			    "bla",           // tooltip
+			    "porcMutacion",                     // campo
+			    0, 100,							     // min y max, aplicando factor, si hay; vale usar Double.*_INFINITY) 
+			    100))								 // opcional: factor de multiplicacion != 1.0, para mostrar porcentajes
+		.addOption(new ConfigPanel.IntegerOption<Modelo>(  // -- entero
+				"Semilla",						     // texto a usar como etiqueta del campo
+				"bla",       // texto a usar como 'tooltip' cuando pasas el puntero
+				"semilla",						     // campo (espera que haya un getGrosor y un setGrosor)
+				0, Integer.MAX_VALUE))			     // min y max (usa Integer.MIN_VALUE /MAX_VALUE para infinitos)
+		
+		/*.addOption(new ConfigPanel.StrategyOption<Modelo>( // -- eleccion de objeto configurable
 				"forma",							 // etiqueta
 				"forma de la figura",                // tooltip
 				"forma",                             // campo
 				formas))                             // elecciones (deben implementar Cloneable)
 				
 			  // para cada clase de objeto interno, hay que definir sus opciones entre un beginInner y un endInner 
-			  .beginInner(new ConfigPanel.InnerOption<Caca,Forma>( 
+			  .beginInner(new ConfigPanel.InnerOption<Modelo,Forma>( 
 			  	"rectangulo", "opciones del rectangulo", "forma", Rectangulo.class))
 		  		  .addInner(new ConfigPanel.DoubleOption<Forma>(
 		  		     "ancho", "ancho del rectangulo", "ancho", 0, Double.POSITIVE_INFINITY))
 		  		  .addInner(new ConfigPanel.DoubleOption<Forma>(
 		  		     "alto", "alto del rectangulo", "alto", 0, Double.POSITIVE_INFINITY))
-		  	  .endInner()
+		  	  .endInner()*/
+		
 		.endOptions();
 		
 		return config;
 	}
 	
-	public class Caca {
-		int a;
-		double b;
-		private int grosor = 1;
-		private String color;
-		private Forma forma;
+	public class Modelo {
+		private String funcion;
+		private Double precision;
+		private int poblacion;
+		private int generaciones;
+		private double porcCruces;
+		private double porcMutacion;
+		private int semilla;
 		
-		public int getGrosor() {
-			return grosor;
-		}
-		public void setGrosor(int grosor) {
-			this.grosor = grosor;
+		public Modelo() {
+			this.funcion = new String("función 1");
+			this.precision = 0.001;
+			this.poblacion = 100;
+			this.generaciones = 100;
+			this.porcCruces = 0.6;
+			this.porcMutacion = 0.05;
+			this.semilla = 0;
 		}
 		
-		public String getColor() {
-			return color;
+		public String getFuncion() {
+			return funcion;
 		}
-		public void setColor(String color) {
-			this.color = color;
+
+		public void setFuncion(String funcion) {
+			this.funcion = funcion;
 		}
-		public Forma getForma() {
-			return forma;
+		
+		public double getPrecision() {
+			return precision;
 		}
-		public void setForma(Forma forma) {
-			this.forma = forma;
+
+		public void setPrecision(double precision) {
+			this.precision = precision;
 		}
+
+		public int getPoblacion() {
+			return poblacion;
+		}
+
+		public void setPoblacion(int poblacion) {
+			this.poblacion = poblacion;
+		}
+
+		public int getGeneraciones() {
+			return generaciones;
+		}
+
+		public void setGeneraciones(int generaciones) {
+			this.generaciones = generaciones;
+		}
+
+		public double getPorcCruces() {
+			return porcCruces;
+		}
+
+		public void setPorcCruces(double porcCruces) {
+			this.porcCruces = porcCruces;
+		}
+
+		public double getPorcMutacion() {
+			return porcMutacion;
+		}
+
+		public void setPorcMutacion(double porcMutacion) {
+			this.porcMutacion = porcMutacion;
+		}
+
+		public int getSemilla() {
+			return semilla;
+		}
+
+		public void setSemilla(int semilla) {
+			this.semilla = semilla;
+		}
+
 		public String toString() {
-			return "grosor: " + this.grosor;
+			return "poner bien";
 		}
 	}
 	
@@ -194,4 +274,5 @@ public class MainWindow extends JFrame {
 			return "rect. de " + ancho  + "x" + alto; 
 		}
 	}
+	
 }
