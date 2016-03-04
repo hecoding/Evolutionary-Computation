97b5dac692e82dbb1bae5608c229e332584499c7
hecoding <hlaria@ucm.es>
2016-03-04 06:18:46 +0100

    Genetic algorithm implementation beginning

---
+++ src/model/GeneticAlgorithm.java
@@ -0,0 +1,82 @@
+package model;
+
+import java.util.ArrayList;
+
+import model.Chromosome.BooleanChromosome;
+
+public class GeneticAlgorithm { // TODO make it generic
+	private ArrayList<BooleanChromosome> population;
+	private int maxGenerationNum;
+	private BooleanChromosome bestChromosome;
+	private double crossProb;
+	private double mutationProb;
+	private double tolerance;
+	private long seed;
+
+	public GeneticAlgorithm(int populationNum, int maxGenerationNum, double crossProb, double mutationProb, double tolerance, long seed) {
+		super();
+		this.population = new ArrayList<BooleanChromosome>(populationNum);
+		this.maxGenerationNum = maxGenerationNum;
+		this.bestChromosome = null;
+		this.crossProb = crossProb;
+		this.mutationProb = mutationProb;
+		this.tolerance = tolerance;
+		this.seed = seed;
+	}
+	
+	public void initialize() {
+		for (int i = 0; i < this.population.size(); i++) {
+			BooleanChromosome chr = new BooleanChromosome(-250, 250, this.tolerance, this.seed);
+			chr.initialize();
+			chr.setAptitude(chr.evaluate());
+			this.population.set(i, chr);
+		}
+	}
+	
+	public void evaluatePopulation() {
+		double currentAggregateScore = 0;
+		double currentBestAptitude = 0;
+		double currentAptitudeSum = 0;
+		BooleanChromosome currentBest = null;
+		
+		for (BooleanChromosome chromosome : this.population) {
+			double currentAptitude = chromosome.getAptitude();
+			currentAptitudeSum += currentAptitude;
+			
+			if (currentAptitude > currentBestAptitude) {
+				currentBest = chromosome;
+				currentBestAptitude = currentAptitude;
+			}
+		}
+		
+		for (BooleanChromosome chromosome : this.population) {
+			chromosome.setScore(chromosome.getAptitude() / currentAptitudeSum);
+			chromosome.setAggregateScore(chromosome.getScore() + currentAggregateScore);
+			currentAggregateScore += chromosome.getScore();
+		}
+		
+		if (currentBestAptitude > this.bestChromosome.getAptitude()) {
+			this.bestChromosome = currentBest;
+		}
+		// generalize for minimization handling also
+	}
+	
+	public boolean finished() {
+		return true;
+	}
+	
+	/* GENETIC OPERATORS */
+	
+	public void selection() {
+		
+	}
+	
+	public void reproduction() {
+		
+	}
+	
+	public void mutation() {
+		
+	}
+
+}
