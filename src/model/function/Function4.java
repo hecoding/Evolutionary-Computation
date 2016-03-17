83b0be5370fc32c62fb297ad29bc25073765ea9c
hecoding <hlaria@ucm.es>
2016-03-17 11:01:37 +0100

    Fix function 4

--- src/model/function/Function4.java
+++ src/model/function/Function4.java
@@ -20,7 +20,7 @@ public class Function4 extends Function {
 		double res = 0;
 		
 		for (int i = 1; i <= this.varNum; i++) {
-			res += Math.sin(params.get(i-1)) * Math.pow(( ((i + 1) * Math.pow(params.get(i-1),2)) / Math.PI ), 20);
+			res += Math.sin(params.get(i-1)) * Math.pow(Math.sin(( ((i + 1) * Math.pow(params.get(i-1),2)) / Math.PI )), 20);
 		}
 		
 		return -res;
