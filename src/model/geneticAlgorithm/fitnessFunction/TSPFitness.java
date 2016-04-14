95a647a08cdf4b584ebebedb453461a6562d86c2
hecoding <hlaria@ucm.es>
2016-04-14 11:25:56 +0200

    Add fitness function

--- src/model/geneticAlgorithm/fitnessFunction/TSPFitness.java
+++ src/model/geneticAlgorithm/fitnessFunction/TSPFitness.java
@@ -2,24 +2,32 @@ package model.geneticAlgorithm.fitnessFunction;
 
 import java.util.ArrayList;
 
+import model.geneticAlgorithm.TSPGeneticAlgorithm;
+
 public class TSPFitness implements FitnessFunctionInterface {
 
 	@Override
 	public double f(ArrayList<Double> params) {
-		// TODO Auto-generated method stub
-		return 0;
+		int distance = 0;
+		
+		for (int i = 0; i < params.size() - 1; i++) {
+			int city1 = params.get(i).intValue();
+			int city2 = params.get(i + 1).intValue();
+			
+			distance += TSPGeneticAlgorithm.Cities.distance(city1, city2);
+		}
+		
+		return distance;
 	}
 
 	@Override
 	public boolean isMinimization() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+		return "TSP Fitness";
 	}
 
 }
