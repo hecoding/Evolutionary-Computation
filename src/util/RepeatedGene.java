7cf70a1e51378da9941f50824a13d1e03d0419c9
Usuario Local <Usuario Local>
2016-04-19 14:01:48 +0200

    Add repeated gene checker

---
+++ src/util/RepeatedGene.java
@@ -0,0 +1,29 @@
+package util;
+
+import java.util.HashMap;
+
+import model.chromosome.TSPChromosome;
+import model.gene.AbstractGene;
+import model.gene.IntegerGene;
+
+public class RepeatedGene {
+	
+	public boolean uniqueGenes(TSPChromosome tspChromosome) {
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		
+		for (AbstractGene<?> gene : tspChromosome.getGenotype()) {
+			int geneInfo = ((IntegerGene) gene).getInformation().intValue();
+			if(!map.containsKey(geneInfo))
+				map.put(geneInfo, 1);
+			else
+				map.put(geneInfo, map.get(geneInfo) + 1);
+		}
+		
+		for (Integer val : map.values()) {
+			if(val > 1)
+				return false;
+		}
+		
+		return true;
+	}
+}
