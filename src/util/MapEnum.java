3876ddd68028d79f4b9edfac3561d9acb83f3491
Usuario Local <Usuario Local>
2016-05-09 14:52:42 +0200

    Add parser

---
+++ src/util/MapEnum.java
@@ -0,0 +1,5 @@
+package util;
+
+public enum MapEnum {
+	beginning, nothing, food, eatenfood, trail
+}
---
+++ src/util/MapParser.java
@@ -0,0 +1,45 @@
+package util;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MapParser {
+	
+	public static Map parse(String fname) throws IOException {
+		Map ret = new Map();
+		BufferedReader br=new BufferedReader(new FileReader(fname));
+		String thisLine;
+		
+        while ((thisLine = br.readLine()) != null) {
+        	ArrayList<MapEnum> row = new ArrayList<>();
+        	
+           for (String cell : thisLine.split(" ")) {
+        	   if(cell.equals("@"))
+        		   row.add(MapEnum.beginning);
+        	   else if (cell.equals("#"))
+        		   row.add(MapEnum.food);
+        	   else
+        		   row.add(MapEnum.nothing);
+           }
+           ret.add(row);
+        }
+		
+		br.close();
+		return ret;
+	}
+	
+	public static class Map extends ArrayList<ArrayList<MapEnum>> {
+		private static final long serialVersionUID = 1L;
+		
+		public int getRows() {
+			return this.size();
+		}
+		
+		public int getColumns() {
+			return this.get(0).size();
+		}
+	}
+	
+}
