117d7732ee8d2e7c4aa8b20a92617080994e8f1b
hecoding <hlaria@ucm.es>
2016-05-15 02:21:05 +0200

    Add ant movement

--- src/model/Ant.java
+++ src/model/Ant.java
@@ -9,6 +9,66 @@ public class Ant {
 		this.pos = new Position(0,0);
 	}
 	
+	public void turnLeft() {
+		switch(this.direction) {
+		case down:
+			this.direction = Direction.right;
+			break;
+		case left:
+			this.direction = Direction.down;
+			break;
+		case right:
+			this.direction = Direction.up;
+			break;
+		case up:
+			this.direction = Direction.left;
+			break;
+		}
+	}
+	
+	public void turnRight() {
+		switch(this.direction) {
+		case down:
+			this.direction = Direction.left;
+			break;
+		case left:
+			this.direction = Direction.up;
+			break;
+		case right:
+			this.direction = Direction.down;
+			break;
+		case up:
+			this.direction = Direction.right;
+			break;
+		}
+	}
+	
+	public void moveForward() {
+		switch(this.direction) {
+		case down:
+			this.pos.y++;
+			break;
+		case left:
+			this.pos.x--;
+			break;
+		case right:
+			this.pos.x++;
+			break;
+		case up:
+			this.pos.y--;
+			break;
+		}
+	}
+	
+	public Position getPosition() {
+		return this.pos;
+	}
+	
+	public void setPosition(int x, int y) {
+		this.pos.x = x;
+		this.pos.y = y;
+	}
+	
 	public enum Direction {
 		up, down, right, left
 	}
