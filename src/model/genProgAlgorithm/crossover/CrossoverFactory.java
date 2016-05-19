d737dfa84eade0628e4909f533ba7e1c9e82be3a
hecoding <hlaria@ucm.es>
2016-05-19 06:35:50 +0200

    Add two-point crossover

--- src/model/genProgAlgorithm/crossover/CrossoverFactory.java
+++ src/model/genProgAlgorithm/crossover/CrossoverFactory.java
@@ -2,7 +2,7 @@ package model.genProgAlgorithm.crossover;
 
 public class CrossoverFactory {
 	private static CrossoverFactory instance;
-	private static String[] strategies = {"Un punto"};
+	private static String[] strategies = {"Un punto", "Dos puntos"};
 	
 	private CrossoverFactory() {}
  	
@@ -16,6 +16,8 @@ public class CrossoverFactory {
 	public CrossoverInterface create(String id) {
 		if (id == "Un punto")
 			return new OnePointCrossover();
+		else if(id == "Dos puntos")
+			return new TwoPointCrossover();
 		else
 			throw new IllegalArgumentException("Unknown crossover method");
 	}
