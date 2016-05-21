a5adc28dcd546ea58af2c449bca0b89088998239
hecoding <hlaria@ucm.es>
2016-05-21 13:00:41 +0200

    Refactor initialization

--- src/model/genProgAlgorithm/initialization/FullInitialization.java
+++ src/model/genProgAlgorithm/initialization/FullInitialization.java
@@ -1,36 +1,14 @@
 package model.genProgAlgorithm.initialization;
 
-import java.util.ArrayList;
-
-import model.chromosome.AbstractChromosome;
-import model.chromosome.AntTrailChromosome;
-import model.genProgAlgorithm.fitnessFunction.FitnessFunctionInterface;
 import model.program.Function;
 import model.program.Node;
 import model.program.Terminal;
 import util.RandGenerator;
 import util.Tree;
 
-public class FullInitialization implements InitializationInterface {
-
-	@Override
-	@SuppressWarnings("unchecked")
-	public <T extends AbstractChromosome> ArrayList<T> initialize(int populationSize, FitnessFunctionInterface function, int programHeight, int maxSteps) {
-		ArrayList<AntTrailChromosome> population = new ArrayList<AntTrailChromosome>(populationSize);
-		for (int i = 0; i < populationSize; i++)  {
-			AntTrailChromosome chromosome = new AntTrailChromosome(function, maxSteps, programHeight);
-			Tree<Node> program = new Tree<>();
-			initialize(program, programHeight);
-			
-			chromosome.setProgram(program);
-			chromosome.setAptitude(chromosome.evaluate());
-			population.add(chromosome);
-		}
-		
-		return (ArrayList<T>) population;
-	}
+public class FullInitialization extends InitializationAbstract {
 	
-	private void initialize(Tree<Node> program, int programLevels) {
+	public void initialize(Tree<Node> program, int programLevels) {
 		if(programLevels == 1)
 			program.setValue(Terminal.values()[RandGenerator.getInstance().nextInt(Terminal.values().length)]);
 		
